b92e1f845f6c6d0cd630ed619d2d7fa9216435cf
Lennart <lennart.ziehm@outlook.de>
2025-09-20 22:57:42 +0200

    Added Solution for Problem2

--- src/SelfAssignment1/Problem2/Problem2.java
+++ src/SelfAssignment1/Problem2/Problem2.java
@@ -2,6 +2,18 @@ package SelfAssignment1.Problem2;
 
 public class Problem2 {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        int nValues = 50;
+        // defined the outer for-loop to make it skippable
+        outer:
+        for (int i = 2; i <= nValues; i++) {
+            // Changed to Math.sqrt for the square root and to lesser equals to include the square root itself
+            for (int j = 2; j <= Math.sqrt(i); j++) {
+                if (i % j == 0) {
+                    // Skip the outer loop, because it is no prime anyway
+                    continue outer;
+                }
+            }
+            System.out.println(i);
+        }
     }
 }
