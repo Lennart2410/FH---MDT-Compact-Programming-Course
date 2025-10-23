ee198c6bccabbdd03bb97732f35eac2dc199c4b1
Marianne Nosseir <marianne.wazif@gmail.com>
2025-10-23 23:26:11 +0200

    Fix Station class

--- src/CapstoneProject/StationsTaskManager/Station.java
+++ src/CapstoneProject/StationsTaskManager/Station.java
@@ -17,7 +17,7 @@ public abstract class Station implements IStation {
         this.out = out;
     }
 
-    /** Non-overrideable worker loop (Template Method pattern). */
+
     @Override
     public final void run() {
         try {
@@ -30,17 +30,12 @@ public abstract class Station implements IStation {
 
             }
         } catch (InterruptedException stop) {
-            Thread.currentThread().interrupt();   // graceful shutdown
+            Thread.currentThread().interrupt();
         } catch (Exception e) {
             // As a safety net, if a station throws, convert task to EXCEPTION
-            // In practice you’ll also log and/or send to a dead-letter queue.
             e.printStackTrace();
         }
     }
 
-    /**
-     * Implement this in concrete stations to transform the task
-     * (e.g., PICKING -> PACKING, add payload, etc.).
-     */
     protected abstract Task process(Task task) throws Exception;
 }
