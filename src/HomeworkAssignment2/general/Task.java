4e4c98c1b3a4e7ae82059c3c8e998e79f67b2fe4
Lennart2410 <lennart.ziehm@outlook.de>
2025-10-26 22:03:56 +0100

    Added working project for Homework assignment 2

--- src/HomeworkAssignment2/general/Task.java
+++ src/HomeworkAssignment2/general/Task.java
@@ -15,7 +15,7 @@ public abstract class Task {
      * Create a new task at a given status
      */
     public Task(Order order) {
-        this.id = "TSK"+UUID.randomUUID();
+        this.id = "TSK00"+UUID.randomUUID();
         this.order = order;
     }
 
