fa302c78954fe3ef9d7181306eeaa5acb89a70f2
Lennart2410 <lennart.ziehm@outlook.de>
2025-10-26 22:23:04 +0100

    Added OrderStatus as enum

--- src/HomeworkAssignment1/general/Order.java
+++ src/HomeworkAssignment1/general/Order.java
@@ -9,8 +9,10 @@ public class Order {
     private final List<Item> items;
     private final String destination;
     private final String orderNumber;
+    private final OrderStatusEnum orderStatusEnum;
 
     public Order(String destination, List<Item> items) {
+        this.orderStatusEnum = OrderStatusEnum.ORDERED;
         this.orderNumber = "ORD00" + UUID.randomUUID();
         this.destination = destination;
         this.items = items;
