1d0adec7a17e1f0caacbef18b5ff843d6184cc55
Lennart <lennart.ziehm@outlook.de>
2025-10-31 00:43:19 +0100

    Added Testcases

---
+++ test/HomeworkAssignment1/loading/LoadingStationTest.java
@@ -0,0 +1,37 @@
+package HomeworkAssignment1.loading;
+
+import org.junit.jupiter.api.Test;
+
+public class LoadingStationTest {
+
+    // Test 1 Dock Vehicle into Bay
+    @Test
+    public void dockVehicleIntoBayTest(){
+
+    }
+
+
+    // Test 2 Dock Vehicle into Bay Exception
+    @Test
+    public void dockVehicleIntoBayTest_NoMoreBayException(){
+
+    }
+
+    // Test 3 Start delivery by id
+    @Test
+    public void startDeliveryById(){
+
+    }
+
+    // Test 4 Start delivery by id deliverynotfound exception
+    @Test
+    public void startDeliveryById_DeliveryNotFoundException(){
+
+    }
+
+    // Test 5 Start delivery by id noemployee exception
+    @Test
+    public void startDeliveryById_NoEmployeeException(){
+
+    }
+}
