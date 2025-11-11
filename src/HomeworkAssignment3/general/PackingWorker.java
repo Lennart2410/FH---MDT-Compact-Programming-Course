4128674ee89ad03e9e79ca2fb4e634958b0f6c4c
Marianne Nosseir <marianne.wazif@gmail.com>
2025-11-11 12:10:02 +0100

    - Replace packing current run() with the worker-pool version

--- src/HomeworkAssignment3/general/PackingWorker.java
+++ src/HomeworkAssignment3/general/PackingWorker.java
@@ -3,18 +3,14 @@ package HomeworkAssignment3.general;
 public class PackingWorker {
 
 
-    public enum WorkerType { EMPLOYEE, MACHINE }
-
     private final String workerId;                 // e.g.,"M-1"
     private final long baseMs;                     // base processing time
     private final double speedFactor;// <1 faster, >1 slower
-    private boolean currentlyOccupied;
     public PackingWorker(String workerId, long baseMs, double speedFactor) {
         this.workerId = workerId;
         this.baseMs = baseMs;
         this.speedFactor = speedFactor;
     }
-
 
     public String getWorkerId() {
         return workerId;
@@ -26,14 +22,6 @@ public class PackingWorker {
 
     public double getSpeedFactor() {
         return speedFactor;
-    }
-
-    public boolean isCurrentlyOccupied() {
-        return currentlyOccupied;
-    }
-
-    public void setCurrentlyOccupied(boolean currentlyOccupied) {
-        this.currentlyOccupied = currentlyOccupied;
     }
 
 }
